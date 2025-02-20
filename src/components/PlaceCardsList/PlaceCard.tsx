93aed1576f81a25a5cb91e3f792b9e8b2cfdb89e
Jhoan Zamora <jhoan.zamora@wizeline.com>
2025-02-20 14:43:54 -0500

    hotfix(Place Card) Improve descriptor display

--- src/components/PlaceCardsList/PlaceCard.tsx
+++ src/components/PlaceCardsList/PlaceCard.tsx
@@ -3,11 +3,18 @@ import Image from "next/image";
 import Link from "next/link";
 import { PHOTO_WIDTH, UNKNOWN_LOCATION } from ".";
 
+const capitalize = (s: string) =>
+  (s && String(s[0]).toUpperCase() + String(s).slice(1)) || "";
+
 interface PlaceCardsProps {
   place: PlaceInfo;
 }
 
 export default function PlaceCard({ place }: PlaceCardsProps) {
+  const formattedTypes = place?.details?.types?.map((type) => {
+    return capitalize(type.replaceAll("_", " "));
+  });
+
   return (
     <article>
       <div className="bg-white shadow-lg rounded-lg overflow-hidden">
@@ -30,8 +37,8 @@ export default function PlaceCard({ place }: PlaceCardsProps) {
             <h3 className="text-lg font-semibold">{place.name}</h3>
           )}
           <p className="text-gray-600">{place?.details?.editorialSummary}</p>
-          {!!place?.details?.types?.length && (
-            <p className="text-gray-600">{String(place?.details?.types)}</p>
+          {!!formattedTypes && (
+            <p className="text-gray-600">{formattedTypes.join(", ")}</p>
           )}
           <p className="text-xs text-gray-500">{`Lat: ${place.coordinates.lat}, Lng: ${place.coordinates.lng}`}</p>
         </div>
